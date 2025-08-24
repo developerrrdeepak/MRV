94ea285a09a686eab2b3f3f8fa2512fc0a5e9280
developerrrdeepak <developerrdeepak@gmail.com>
2025-08-24 12:11:41 +0530

    Revert "Fix Netlify authentication issues with debug logging"

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,19 +1,5 @@
 import serverless from "serverless-http";
+
 import { createServer } from "../../server";
 
-// Create the Express app
-const app = createServer();
-
-// Add some logging for debugging
-app.use((req, res, next) => {
-  console.log(`🌐 [NETLIFY API] ${req.method} ${req.path}`, {
-    headers: req.headers,
-    body: req.method !== "GET" ? req.body : undefined,
-  });
-  next();
-});
-
-// Export the serverless handler
-export const handler = serverless(app, {
-  binary: false,
-});
+export const handler = serverless(createServer());
